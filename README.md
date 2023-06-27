ab880f2ee9f3a66e0cf3051cf47e9f0d62d5fa45
Tom Juntunen <tom@datalogz.io>
2023-06-26 23:21:51 -0700

    adding llm.py, updating readme and requirements, add to .gitignore

--- README.md
+++ README.md
@@ -1,22 +1,58 @@
-# medagi
 LangChain Medical Patient Summary
 
 This project reads a patient's FHIR-formatted JSON record, extracts relevant information, and uses OpenAI's language model to create a summary of the patient's information. The summary is then sent as a message over a Pusher channel.
 
 How it works
 
-The project first reads a FHIR-formatted JSON file that contains a patient's medical record.
-The text from this file is split into chunks of 1000 characters each.
-The Chroma class, powered by OpenAI embeddings, is used to convert these text chunks into vectors.
-A question-answering chain is loaded using the OpenAI language model.
-A query is then made to this chain to generate a summary of the patient's information. The output of this query is a summary text.
-This summary is then sent as a message over a Pusher channel, 'General', with the event name 'new-message'.
-The message is then received on the same channel using the pusherclient library.
+The project first reads a FHIR-formatted JSON file that contains a patient's medical record. The text from this file is split into chunks of 1000 characters each. The Chroma class, powered by OpenAI embeddings, is used to convert these text chunks into vectors. A question-answering chain is loaded using the OpenAI language model. A query is then made to this chain to generate a summary of the patient's information. The output of this query is a summary text. This summary is then sent as a message over a Pusher channel named 'General' with the event name 'new-message'. The message is received on the same channel using the pusherclient library.
+
 Requirements
 
-Python 3.x
-pusher
-pusherclient
-langchain
-openai
+- Python 3.x
+- pusher
+- pusherclient
+- langchain
+- openai
+
 Please make sure you have all the required Python packages installed. You can install these packages using pip:
+
+pip install pusher pusherclient langchain openai
+
+Quick Start Guide
+
+1. Clone the repository:
+`https://github.com/tom-juntunen/medagi.git`
+
+2. Navigate to the project directory:
+`cd langchain-medical-patient-summary`
+
+3. Install the required Python packages:
+`pip install -r requirements.txt`
+
+4. Prepare the patient data:
+- Ensure you have a FHIR-formatted JSON file containing the patient's medical record. Let's assume the file is named `patient.json`.
+- Place the `patient.json` file in the project directory.
+
+5. Run the application:
+`python llm.py`
+
+6. Follow the prompts in the command line interface:
+- When prompted, describe your difficulties and the challenges you are facing.
+- If prompted again, provide any other difficulties.
+- The application will generate a summary of the patient's information based on your input.
+
+7. Observe the output:
+- The generated summary will be displayed in the command line interface.
+- The summary will also be sent as a message over the Pusher channel named 'General' with the event name 'new-message'.
+
+Congratulations! You have successfully run the LangChain Medical Patient Summary application. Feel free to explore the code and make any necessary changes to suit your requirements.
+
+Before proceeding with the code update, please note that you will need an OpenAI account and an OpenAI API access token to use the OpenAI models. Here are the steps to acquire the necessary credentials:
+
+Create an OpenAI account: If you don't have an account, visit the OpenAI website (https://www.openai.com) and sign up for an account.
+Generate an OpenAI API access token:
+Log in to your OpenAI account.
+Navigate to the API section of your account dashboard.
+Follow the instructions provided by OpenAI to generate an API access token.
+Take note of the generated API access token, as you will need it to authenticate your API requests.
+Once you have obtained your OpenAI API access token, you can proceed with the code update. Replace the following line:
--- llm.py
+++ llm.py
@@ -1,82 +1,68 @@
+import time
+
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
-import os
 
-from pusher import Pusher
-import time
-import os
 
-# Read JSON file
-with open('patient.json', 'r') as file:
-    text = file.read()
+def read_patient_data(file_path):
+    with open(file_path) as f:
+        return f.read()
 
-with open("patient.json") as f:
-    patient_data = f.read()
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_text(patient_data)
+def split_text_into_chunks(text, chunk_size, chunk_overlap):
+    text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    return text_splitter.split_text(text)
 
-embeddings = OpenAIEmbeddings()
 
-query1 = 'Return me one single patient record.'
-docsearch = Chroma.from_texts(texts, embeddings, metadatas=[{"source": str(i)} for i in range(len(texts))]).as_retriever()
-docs = docsearch.get_relevant_documents(query1)
-docs[0].page_content = docs[0].page_content[:2500]
+def load_embeddings():
+    return OpenAIEmbeddings()
 
-chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
-query2 = f"""You are medical expert who also knows JSON and FHIR protocol. 
-Summarise the info about the patient based on the given FHIR JSON data."""
+def get_relevant_documents(texts, embeddings):
+    query = 'Return me one single patient record.'
+    docsearch = Chroma.from_texts(texts, embeddings, metadatas=[{"source": str(i)} for i in range(len(texts))]).as_retriever()
+    docs = docsearch.get_relevant_documents(query)
+    docs[0].page_content = docs[0].page_content[:2500]  # limit token count
+    return docs
 
-msg = chain.run(input_documents=docs, question=query2)
 
-# instantiate the Pusher class with your appId, key, secret and cluster
-PUSHER_APP_ID = os.environ["PUSHER_APP_ID"]
-PUSHER_API_KEY = os.environ["PUSHER_API_KEY"]
-CLUSTER = 'us3'
-PUSHER_SECRET = os.environ["PUSHER_SECRET"]
+def load_question_answering_chain():
+    return load_qa_chain(OpenAI(model='text-davinci-003'), chain_type="stuff")
 
-pusher_client = Pusher(
-  app_id=PUSHER_APP_ID,
-  key=PUSHER_API_KEY,
-  secret=PUSHER_SECRET,
-  cluster=CLUSTER,
-  ssl=True
-)
 
-# trigger an event to a channel. channel-name and event-name can be custom
-pusher_client.trigger('General', 'new-message', 
-                      {'id': 'message-123', 'user': 'medAGI', 'message': msg}
-)
+def run_interview(docs, chain, query, interview):
+    msg = chain.run(input_documents=docs, question=query + ' ' + interview)
+    return msg
 
-def callback(data):
-    print('received %s' % data)
 
-import pusherclient
-import time
+if __name__ == "__main__":
+    file_path = "patient.json"
+    patient_data = read_patient_data(file_path)
 
-# replace with your pusher key
-pusherkey = PUSHER_API_KEY
-pusher_c = pusherclient.Pusher(key=pusherkey, secret=PUSHER_SECRET)
+    chunk_size = 1000
+    chunk_overlap = 0
+    texts = split_text_into_chunks(patient_data, chunk_size, chunk_overlap)
 
-# define callback function to process event
-def print_callback(data):
-    print(data)
+    embeddings = load_embeddings()
 
-def connect_handler(data):
-    try:
-        channel = pusher_c.subscribe('General')
-        channel.bind('new-message', print_callback)
-    except RuntimeError as e:
-        print(f"RuntimeError occurred: {e}")
-        # handle the error here or just pass
+    docs = get_relevant_documents(texts, embeddings)
 
-pusher_c.connection.bind('pusher:connection_established', connect_handler)
-pusher_c.connect()
+    chain = load_question_answering_chain()
 
-while True:
-    # Keep the main thread alive and the connection with Pusher persistent
-    time.sleep(1)
+    query = """You are a medical expert who also knows JSON and FHIR protocol. 
+    Read the info about the patient based on the given FHIR JSON data and this additional info. 
+    Ask open-ended questions about the location of the problem, how long it has occurred, 
+    and how severe the issue is as if you are speaking with the patient directly."""
+
+    i = 0
+    msg = ''
+    while True:
+        q = "Describe your difficulties and the challenges you are facing:\n"
+        q2 = f"{msg}:\n"
+        interview = input(q if i == 0 else q2)
+        msg = run_interview(docs, chain, query, interview)
+        i += 1
+        time.sleep(3)
